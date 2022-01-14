7f9b352124d5f2cffad66614276b27e3bb2699b4
Sebastian Łuszczek <sebastianluszczek@pop-os.localdomain>
2022-01-15 00:20:06 +0100

    push to ECR workflow & ECR tf

---
+++ terraform/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region ="eu-central-1"
+}
+
+resource "aws_ecr_repository" "ecr" {
+  name                 = "gonotes"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+ }
+  
+  tags = {
+   Env = "dev"
+ }
+
+}
